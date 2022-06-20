de9ed82004160c5c56e6e6a9719ae7f8606e8e6c
six <quocsy2511@gmail.com>
2022-06-20 23:22:55 +0700

    update

--- Lost_and_Found/src/java/controller/HomeController.java
+++ Lost_and_Found/src/java/controller/HomeController.java
@@ -47,7 +47,6 @@ public class HomeController extends HttpServlet {
 
                 } else {
                     listArticles = articleDao.getListArticleByLocation(searchByLocation);
-
                 }
             }
             if (searchByType == null) {
