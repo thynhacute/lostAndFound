d922ec3c6f6e676689c042fedeafaef6a728c49c
six <quocsy2511@gmail.com>
2022-06-30 10:07:38 +0700

    send mail report

--- Lost_and_Found/src/java/javatutorials/javamail/JavaMailUtil.java
+++ Lost_and_Found/src/java/javatutorials/javamail/JavaMailUtil.java
@@ -58,5 +58,56 @@ public class JavaMailUtil {
         System.out.println("Message sent Successfuly ");
 
     }
+//------------------------------------------------------ end warning mail
+    
+    
+        private static Message prepareMessageBan(Session session, String myAccountEmail, String recepient, String fullName) {
 
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(myAccountEmail));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+            message.setSubject(" Ban Email !!!  ");
+            String htmlCode = "<h1 style=\"color: orange;\"> Lost and Found  </h1> \n "
+                    + "<h2>Hi, " + fullName + " </h2>\n"
+                    + "<h3> we received many complaints from other users after review we have decided to temporarily lock your account </h3> \n"
+                    + "<h4> the following are possible reasons : </h4> \n"
+                    + "<h5> - Using nudity or pornography. </h5> \n" 
+                    + "<h5> - Using language that incites hatred or intimidation. Or attack a specific person or organization directly. </h5> \n" 
+                    + "<h5> - Using unsafe content or harm yourself.  </h5> \n" 
+                    + "<h5> - Using fake or impersonated accounts.  </h5> \n" 
+                    + "<h5> - Stealing trademarks, copyrights, intellectual property rights.  </h5> \n" 
+                    + "<h5> - Spam. </h5> \n" 
+                    + "<h4>  Any questions or complaints regarding for account deactivation, please reply to this email for consideration ! </h4> \n"
+                    + "<h4>  synqse151029@fpt.edu.vn </h4> \n";
+            message.setContent(htmlCode, "text/html");
+            return message;
+        } catch (Exception ex) {
+            Logger.getLogger(JavaMailUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public static void sendMailBanMember(String recepient, String fullName) throws MessagingException {
+        System.out.println("Preparing to send email ");
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        String myAccountEmail = "synqse151029@fpt.edu.vn";
+        String password = "0983709791ad";
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(myAccountEmail, password);
+            }
+        });
+
+        Message messageBan = prepareMessageBan(session, myAccountEmail, recepient, fullName);
+        Transport.send(messageBan);
+        System.out.println("Message sent Successfuly ");
+
+    }
 }
