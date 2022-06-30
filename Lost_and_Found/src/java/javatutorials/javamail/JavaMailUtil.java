8870c17b0bad33ad4ded23a97200773f10bc953e
six <quocsy2511@gmail.com>
2022-06-30 10:22:19 +0700

    send mail active

--- Lost_and_Found/src/java/javatutorials/javamail/JavaMailUtil.java
+++ Lost_and_Found/src/java/javatutorials/javamail/JavaMailUtil.java
@@ -110,4 +110,51 @@ public class JavaMailUtil {
         System.out.println("Message sent Successfuly ");
 
     }
+    
+//    ---------------------------------- end send mail BAN of admin
+    
+     private static Message prepareMessageActive(Session session, String myAccountEmail, String recepient, String fullName) {
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(myAccountEmail));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+            message.setSubject(" Active Email !!!  ");
+            String htmlCode = "<h1 style=\"color: orange;\"> Lost and Found  </h1> \n "
+                    + "<h2>Hi, " + fullName + " </h2>\n"
+                    + "<h3> we received many complaints from other users after reviewing your account and we decided to reopen it for you </h3> \n"
+                    + "<h4> We deeply apologize for the convenience of your web usage experience </h4> \n"
+                    + "<h4>  Any questions or complaints regarding for account deactivation, please reply to this email for consideration ! </h4> \n"
+                    + "<h2>  THANKS  </h2> \n"
+                    + "<h4>  synqse151029@fpt.edu.vn </h4> \n";
+            message.setContent(htmlCode, "text/html");
+            return message;
+        } catch (Exception ex) {
+            Logger.getLogger(JavaMailUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public static void sendMailActiveMember(String recepient, String fullName) throws MessagingException {
+        System.out.println("Preparing to send email ");
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        String myAccountEmail = "synqse151029@fpt.edu.vn";
+        String password = "0983709791ad";
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(myAccountEmail, password);
+            }
+        });
+
+        Message messageBan = prepareMessageActive(session, myAccountEmail, recepient, fullName);
+        Transport.send(messageBan);
+        System.out.println("Message sent Successfuly ");
+
+    }
 }
