f1d822cffe7f73d0721bc9a9f6cceef0778f021a
six <quocsy2511@gmail.com>
2022-07-11 23:10:11 +0700

    ok

--- Lost_and_Found/src/java/notification/notificationDAO.java
+++ Lost_and_Found/src/java/notification/notificationDAO.java
@@ -118,46 +118,7 @@ public class NotificationDAO {
         return check;
     }
 
-//    public List<NotificationDTO> getListNotificationArticleFind(int memberID) throws SQLException {
-//        List<NotificationDTO> list = new ArrayList<>();
-//        Connection conn = null;
-//        PreparedStatement ptm = null;
-//        ResultSet rs = null;
-//        try {
-//            conn = DBUtils.getConnection();
-//            if (conn != null) {
-//                String sql = "  SELECT N.NotificationID, N.content, N.MemberID, N.SensorID, N.ArticleID, M.FullName, M.Picture "
-//                        + "FROM Notification N\n"
-//                        + "INNER JOIN Member M ON M.MemberID = N.SensorID WHERE N.MemberID = ?";
-//                ptm = conn.prepareStatement(sql);
-//                ptm.setInt(1, memberID);
-//                rs = ptm.executeQuery();
-//                while (rs.next()) {
-//                    int notificationID = rs.getInt("notificationID");
-//                    String content = rs.getString("content");
-//                    int memberID1 = rs.getInt("memberID");
-//                    int sensorID = rs.getInt("sensorID");
-//                    int articleID = rs.getInt("articleID");
-//                    String fullName = rs.getString("fullName");
-//                    String picture = rs.getString("picture");
-//                    list.add(new NotificationDTO(notificationID, content, memberID1, sensorID, articleID, fullName, picture));
-//                }
-//            }
-//        } catch (Exception e) {
-//        } finally {
-//            if (rs != null) {
-//                rs.close();
-//            }
-//            if (ptm != null) {
-//                ptm.close();
-//            }
-//            if (conn != null) {
-//                conn.close();
-//            }
-//        }
-//        return list;
-//
-//    }
+
     public boolean getSeenNoti(String articleID1) throws SQLException {
         boolean check = false;
         Connection conn = null;
